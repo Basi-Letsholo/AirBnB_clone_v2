52a6e78692e00c0b3208cb96b400f0954065704f
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-10-27 17:57:58 +0000

    update

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,8 +25,10 @@ def c_text(text):
     return "C " + text
 
 
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text='is cool'):
+def python_text(text):
+    """ returns custom text"""
     text = text.replace("_", " ")
     return "Python " + text
 
