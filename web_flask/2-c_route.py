0be9b42a88ebe5428502e5f0b750539931c70835
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-10-27 17:49:29 +0000

    c route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Starts flask web app."""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Returns hello hbnb"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def custom_text(text):
+    """returns custom text"""
+    text = text.replace("_", " ")
+    return "C " + text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
